9f85baf9d3fb8cf0270ea59e8f6aa9db43574cae
Ahmed <115215431+Ahmed12410@users.noreply.github.com>
2023-05-27 14:48:17 +0400

    Update Snek.py

--- Snake/Snek.py
+++ Snake/Snek.py
@@ -207,6 +207,7 @@ while True:
                 if main_game.snake.direction.x != 1:
                     main_game.snake.direction = Vector2(-1, 0)
 		
+		
     screen.fill((175,215,70))
     main_game.draw_elements()
     pygame.display.update()
